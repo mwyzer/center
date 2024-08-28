284bf7518f6af7abaede6448e8986004b4640901
mwyzer <muhammad.wyzer@gmail.com>
2024-08-28 14:16:32 +0800

    modify

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-500">I am home page</h1>
+      <h1 className="text-red-500">I am home page 2</h1>
     </div>
   );
 }
