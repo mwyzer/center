4c8bde003452cb5db89f19b3613510d833cc15c0
mwyzer <muhammad.wyzer@gmail.com>
2024-08-28 14:55:06 +0800

    update sore

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,13 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Wilzio Center",
   description: "Integrated App",
 };
+
+
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+ 
 
 export default function RootLayout({
   children,
@@ -15,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function App() {
   return (
     <div>
       <h1 className="text-red-500">I am home page 2</h1>
+      <Button>Test</Button>
     </div>
   );
 }
